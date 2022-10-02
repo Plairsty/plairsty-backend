16ee85a90b7bcb06d4a4d5b6d35cb37b8696b168
YadavGulshan <yadavgulshan542@gmail.com>
2022-10-02 14:14:01 +0530

    Basic Server implementation

--- cmd/api/client/main.go
+++ cmd/api/client/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"log"
+	"net"
+	"os"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port string
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
 
 func main() {
-	log.Println("Hello world!")
+	var cfg config
+	flag.StringVar(&cfg.port, "port", ":4000", "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	_ = &application{
+		config: cfg,
+		logger: logger,
+	}
+	lis, err := net.Listen("tcp", cfg.port)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	log.Println("Starting server on port", cfg.port)
+	defer func(conn net.Listener) {
+		log.Println("Closing server on port", cfg.port)
+		err := conn.Close()
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}(lis)
 }
