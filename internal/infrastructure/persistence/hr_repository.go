c8bc0f945fcc425f2b044427d2152581f133d77f
YadavGulshan <yadavgulshan542@gmail.com>
2022-10-11 11:39:33 +0530

    Added db operations for hr

--- internal/infrastructure/persistence/hr_repository.go
+++ internal/infrastructure/persistence/hr_repository.go
@@ -10,17 +10,53 @@ type hrRepository struct {
 }
 
 func (r hrRepository) Insert(hr *hrPb.Hr) error {
-	panic("implement me")
+	Query := `
+		INSERT INTO hr (name, phone, email, company) 
+		VALUES ($1, $2, $3, $4)
+		RETURNING id
+		`
+	err := r.DB.QueryRow(Query, hr.Name, hr.Phone, hr.Email, hr.Company).Scan(&hr.Email)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (r hrRepository) Get(id int64) (*hrPb.Hr, error) {
-	panic("implement me")
+	Query := `
+		SELECT id, name, phone, email, company
+		FROM hr
+		WHERE id = $1
+		`
+	var hr hrPb.Hr
+	err := r.DB.QueryRow(Query, id).Scan(&hr.Id, &hr.Name, &hr.Phone, &hr.Email, &hr.Company)
+	if err != nil {
+		return nil, err
+	}
+	return &hr, nil
 }
 
 func (r hrRepository) Update(hr *hrPb.Hr) error {
-	panic("implement me")
+	Query := `
+		UPDATE hr
+		SET name = $1, phone = $2, email = $3, company = $4
+		WHERE id = $5
+		`
+	_, err := r.DB.Exec(Query, hr.Name, hr.Phone, hr.Email, hr.Company, hr.Id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (r hrRepository) Delete(id int64) error {
-	panic("implement me")
+	Query := `
+		DELETE FROM hr
+		WHERE id = $1
+		`
+	_, err := r.DB.Exec(Query, id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
