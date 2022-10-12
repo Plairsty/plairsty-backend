2e5e68e5219326dfbfe51571daf6612acf08eaa6
YadavGulshan <yadavgulshan542@gmail.com>
2022-10-12 13:06:06 +0530

    Added migration for cgpa columns

---
+++ migrations/000015_cgpa_column_for_students.up.sql
@@ -0,0 +1,9 @@
+-- 8 columns for all different semesters
+ALTER TABLE students ADD COLUMN semester1 float default 0;
+ALTER TABLE students ADD COLUMN semester2 float default 0;
+ALTER TABLE students ADD COLUMN semester3 float default 0;
+ALTER TABLE students ADD COLUMN semester4 float default 0;
+ALTER TABLE students ADD COLUMN semester5 float default 0;
+ALTER TABLE students ADD COLUMN semester6 float default 0;
+ALTER TABLE students ADD COLUMN semester7 float default 0;
+ALTER TABLE students ADD COLUMN semester8 float default 0;
